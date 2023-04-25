1532807de5b4805f55f9986d8ca9d44425a04748
Sabakhupenia <131442959+Sabakhupenia@users.noreply.github.com>
2023-04-25 18:07:01 +0400

    Create indoor.py

---
+++ indoor.py
@@ -0,0 +1,5 @@
+#this program tells the user to tipe something at all caps
+allcaps = input("TYPE SOMETHING AT ALL CAPS! ")
+#and then converts text to lowercase
+allcaps = allcaps.lower()
+print(allcaps)
