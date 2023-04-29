b98cbf4c628a7b502163c2142e51ea971047f92b
Sabakhupenia <131442959+Sabakhupenia@users.noreply.github.com>
2023-04-29 19:21:51 +0400

    Update deep.py

--- deep.py
+++ deep.py
@@ -1,8 +1,15 @@
+#This function gets userinput then prints answer function
 def main():
-    answer = input("What is the answer to the Great Question of Life, the Universe and Everything? ").strip().lower()
-    match answer:
+    uinput = input("What is the answer to the Great Question of Life, the Universe and Everything? ").strip().lower()
+    print(answer(uinput))
+#This function takes some text and if true returns Yes if false returns No as a string
+#Note: 42 here is a string
+def answer(txt):
+
+    match txt:
         case "42" | "forty-two" | "forty two":
-            print("Yes")
+            return "Yes"
         case _:
-            print("No")
+            return "No"
+
 main()
