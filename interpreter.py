8eb44fa4e85389f5cf4410e51a643e4126d87f35
Sabakhupenia <131442959+Sabakhupenia@users.noreply.github.com>
2023-05-04 13:09:51 +0400

    Update interpreter.py

--- interpreter.py
+++ interpreter.py
@@ -1,22 +1,14 @@
-
-def main():
-
-    expression = input("Expression: ")
-    x, y, z = expression.split(" ")
+def calculate(str):
+    x, y, z = str.split(" ")
     x = float(x)
     z = float(z)
-#this function call takes x,y,z as parameters
-    calculate(x,y,z)
-
-def calculate(x,y,z):
-
     if y == "+":
-        print(float(x) + float(z))
+        return x+z
     elif y == "-":
-        print(float(x) - float(z))
+        return x-z
     elif y == "*":
-        print(float(x) * float(z))
+        return x*z
     elif y == "/":
-        print(float(x) / float(z))
+        return x/z
 
 main()
